0c0ef84ca96d5fcea576552564ebb0d5350dce77
Alan <boyce.alan15@gmail.com>
2021-11-15 21:42:43 -0500

    user controller

--- src/profile/controllers/UserController.ts
+++ src/profile/controllers/UserController.ts
@@ -1,8 +1,21 @@
 import { User } from '../models/User';
 
 export default class UserController {
+  public getUser(userToFind) {
+    return User
+      .findOne(userToFind)
+      .then((response) => {
+        console.log("Success! User created.");
+        return response;
+      })
+      .catch((response) => {
+        console.log("Error! User could not be created.");
+        return response;
+      });
+  }
+
   public create(user) {
-    return user
+    return User
       .create(user)
       .then((response) => {
         console.log("Success! User created.");
